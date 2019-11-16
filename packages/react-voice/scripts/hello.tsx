13295a72e47312127b8aa9998ace17de75a00f8c
Toru Kobayashi <koba0004@gmail.com>
2019-11-17 01:38:27 +0900

    fix: lint errors

--- packages/react-voice/scripts/hello.tsx
+++ packages/react-voice/scripts/hello.tsx
@@ -4,9 +4,15 @@ import { ReactVoice } from "../src";
 ReactVoice.render(
   <>
     <kyoko>こんにちは</kyoko>
-    <alex>I work as a frontend developer for Cybozu and I work as a frontend advisor for SmartHR.</alex>
+    <alex>
+      I work as a frontend developer for Cybozu and I work as a frontend advisor
+      for SmartHR.
+    </alex>
     <alex>My Twitter and GitHub accounts are @koba04, please follow me!</alex>
-    <victoria>I'm also one of the organizers of React.js meetup in Tokyo and a contributor of React.</victoria>
+    <victoria>
+      I'm also one of the organizers of React.js meetup in Tokyo and a
+      contributor of React.
+    </victoria>
     <victoria>I've been working with React for 5years.</victoria>
   </>,
   {}

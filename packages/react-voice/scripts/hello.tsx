9112e0e13928165b3c4084f258a993a57e56c801
Toru Kobayashi <koba0004@gmail.com>
2019-11-17 01:33:18 +0900

    feat(react-voice): use react-voice for introduction

--- packages/react-voice/scripts/hello.tsx
+++ packages/react-voice/scripts/hello.tsx
@@ -3,9 +3,11 @@ import { ReactVoice } from "../src";
 
 ReactVoice.render(
   <>
-    <alex>Hello</alex>
-    <victoria>JSConf JP</victoria>
-    <kyoko>こんにちは みなさん</kyoko>
+    <kyoko>こんにちは</kyoko>
+    <alex>I work as a frontend developer for Cybozu and I work as a frontend advisor for SmartHR.</alex>
+    <alex>My Twitter and GitHub accounts are @koba04, please follow me!</alex>
+    <victoria>I'm also one of the organizers of React.js meetup in Tokyo and a contributor of React.</victoria>
+    <victoria>I've been working with React for 5years.</victoria>
   </>,
   {}
 );
